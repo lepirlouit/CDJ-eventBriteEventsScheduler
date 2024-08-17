2779f03a9f91ba464da29449903865f82de61632
Benoît de Biolley <bdb@aloalto.com>
2024-08-18 00:39:43 +0200

    :construction: Start todo list

---
+++ promoteEvent.js
@@ -0,0 +1,8 @@
+// list events
+// ask which event to promote (show only upcoming published events)
+// copy previous campain
+// update dates in title (X2) and content (X2/3)
+// ask if need to add/enrich list with emails of previous event
+// if yes, list previous events (default is latest)
+// Ask for schedule the campain or to send now
+// done.
