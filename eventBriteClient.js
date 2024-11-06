697653865ddb22f1a34087b6ba39768e57ed066e
Benoît de Biolley <bdb@aloalto.com>
2024-11-06 17:53:44 +0100

    :construction: Wip: promote events

--- eventBriteClient.js
+++ eventBriteClient.js
@@ -1,4 +1,50 @@
 const { dateToDateStr } = require('./dateUtils');
+// const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+module.exports.listYourOrganizations = async () => {
+  const response = await fetch(`https://www.eventbriteapi.com/v3/users/me/organizations/`, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${process.env.EVENTBRITE_Private_token}`,
+      'Content-Type': 'application/json'
+    },
+  });
+  const data = await response.json();
+  if (response.ok) {
+    // const { id: eventId } = data;
+    return data.organizations;
+  }
+
+  throw new Error(`${data.error} - ${data.error_description}`);
+}
+
+module.exports.listEventsByOrganization = async ({ organizationId, status, timeFilter }) => {
+  const queryString = new URLSearchParams({
+    show_series_parent: true,
+    order_by: "start_asc",
+    page: 1,
+    page_size: 20,
+    app_name: "events-workspace",
+    status,
+    time_filter: timeFilter,
+  }).toString();
+  // const queryString = "show_series_parent=true&order_by=start_asc&page=1&page_size=20&app_name=events-workspace&status=live,started,ended,canceled&time_filter=current_future";
+  const response = await fetch(`https://www.eventbriteapi.com/v3/organizations/${organizationId}/events/${queryString ? '?' : ''}${queryString}`, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${process.env.EVENTBRITE_Private_token}`,
+      'Content-Type': 'application/json'
+    },
+  });
+  const data = await response.json();
+  if (response.ok) {
+    // const { id: eventId } = data;
+    return data.events;
+  }
+
+  throw new Error(`${data.error} - ${data.error_description}`);
+}
+
 /**
  * 
  * @param {{originalEventId: string, name: string, startDate: string, endDate: string}} 
@@ -165,5 +211,49 @@ module.exports.updateEventName = async ({ eventId, name }) => {
   throw new Error(`${data.error} - ${data.error_description}`)
 }
 
+module.exports.copyEmailCampaign = async ({ campaignId }) => {
+  const response = await fetch("https://www.eventbrite.be/api/v3/missive/campaigns/29077159/duplicate/", {//} `https://www.eventbriteapi.com/v3/missive/campaigns/${campaignId}/duplicate/`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${process.env.EVENTBRITE_Private_token}`,
+      'Content-Type': 'application/json',
+      'Accept-Encoding': 'gzip',
+      'X-CSRFToken': "whatever",
+      "Cookie": 'mgref=typeins; csrftoken=whatever; stableId=c6d766a9-8a27-45c1-bf61-a224e8edcaf8; G=v%3D2%26i%3Df18348ff-d4ad-4968-995e-355f5dcec7a2%26a%3D1237%26s%3D5570e7d5304e63898f1fe7ac0fe8af54d0d1cddf; eblang=lo%3Dnl_BE%26la%3Dnl-nl; G_ENABLED_IDPS=google; __stripe_mid=061b7577-d0a5-4ac1-899c-f11c8571c93cbe4cd7; mgref=eafil; ajs_user_id=null; ajs_group_id=null; ajs_anonymous_id=%226fe944a1-b791-4e59-9fed-4683484222a8%22; g_state={"i_p":1713519081924,"i_l":1}; location=%7B%22current_place_parent%22%3A%20%22Belgium%22%2C%20%22place_type%22%3A%20%22locality%22%2C%20%22current_place%22%3A%20%22Brussel%22%2C%20%22latitude%22%3A%2050.8509%2C%20%22country%22%3A%20%22Belgium%22%2C%20%22place_id%22%3A%20%22101751573%22%2C%20%22slug%22%3A%20%22belgium--brussel--10471%22%2C%20%22longitude%22%3A%204.3447%7D; mglts=; mglogin2=v%3D3%26uid%3D273908626%26ts%3D1723807664%26n%3Dfnclh7ndsuaxsvxs%26ip%3D185.83.137.249%26p%3D1%26sig%3DAG08tkoBRpeB__BMik404xgMnwZEMdC-tQ; PWE=AFkEbFjo9-e02f6ICg_Q6clyfvKbMRoabqDk4bfyfxXUZbi2fjacw1qMxUOpVS_JqwOPUEoh0tUVSE3cOJWqnIKE2M-nk3EXJl6xZOSJF8lybQT6E17EFHA; mgssl=v%3D1%26sig%3DAIDFc2vZvogv6kaKmIj9aGhqCVoYLq84Qg; mgemail=ANdEhP0VMYIQQ3do8q3oa1sjDn6ZhPuu9J4r8AmnHzEDJHhhmZrvYxPdrcRp8LEUM7s_jwcc6h29; active_organization_id=53624399466; mgaff996602965297=oddtdtcreator; SS=AE3DLHTdSTOEzxdDvttEZftWxN-zw9UBgQ; SP=AGQgbblWB9WIfFQmaoZ5wiD5sL-QAXSErFuNnEjc_e1-mDGGCqQTKnS-qgT6QzJuVduS3vmLOOfFnKRhJ_dgb0Hmwn0PbMJKQtNPq_Kk-oKWnPdNKZ7FbIS6eU0e1N2cG6ERNdjpqlumCkyY8B9d7NviKn4k0YmRZS8d9mARJwKs0O_fkt2VzY4Ec2LTqlCVwC19XJ33y4JJJXHMgyetABXT0KCIhw9uDh3m74NlObTiqhZGn5-8uus; AS=ba7000d5-912d-4296-ad4e-86f80d982b11; django_timezone=Europe/Brussels; __stripe_sid=27d8a618-2996-48e1-8c6e-7b816ebfeb6e5cbac1; _dd_s=rum=0&expire=1725544218102',
+      "Referer": "https://www.eventbrite.be/organizations/campaigns/email",
+    },
+    body: JSON.stringify({
+      locale: "nl_BE"
+    }),
+  });
+  const data = await response.json();
+  if (response.ok) {
+    return;
+  }
+
+  throw new Error(`${data.error} - ${data.error_description}`)
+}
+module.exports.updateCampaign = async ({ campaignId, name, subject, bodyMessage }) => {
+  const response = await fetch(`https://www.eventbriteapi.com/v3/missive/campaigns/${campaignId}/duplicate/`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${process.env.EVENTBRITE_Private_token}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      campaign: {
+        name,
+        subject,
+        body_message: bodyMessage,
+      }
+    }),
+  });
+  const data = await response.json();
+  if (response.ok) {
+    return;
+  }
+
+  throw new Error(`${data.error} - ${data.error_description}`)
+}
 
 
--- promoteEvent.js
+++ promoteEvent.js
@@ -1,8 +1,45 @@
-// list events
-// ask which event to promote (show only upcoming published events)
-// copy previous campain
-// update dates in title (X2) and content (X2/3)
-// ask if need to add/enrich list with emails of previous event
-// if yes, list previous events (default is latest)
-// Ask for schedule the campain or to send now
-// done.
+require('dotenv').config();
+const fs = require("node:fs");
+const handlebars = require("handlebars");
+const eventBriteClient = require("./eventBriteClient");
+
+const CAMPAIGN_TO_DUPPLICATE = "29077159"; // CoderDojo 1190 Forest - 08/06/2024 Email Campaign
+
+const main = async () => {
+  const organizations = await eventBriteClient.listYourOrganizations();
+  if (organizations.length > 1) {
+    throw new Error("More than one organization for this account");
+  }
+  const organization = organizations[0];
+  // list events
+  const eventsList = await eventBriteClient.listEventsByOrganization({ organizationId: organization.id, status: "live,started,ended,canceled", timeFilter: "current_future" });
+  if (eventsList.length > 1) {
+    throw new Error("More than one upcoming event");
+  }
+  const event = eventsList[0];
+  console.log(JSON.stringify(event, null, 2));
+  // (ask which event to promote (show only upcoming published events))
+  // copy previous campaign
+  // const campaignCopy = await eventBriteClient.copyEmailCampaign({ campaignId: CAMPAIGN_TO_DUPPLICATE });
+  const dateShort = new Intl.DateTimeFormat('fr-BE').format(new Date(event.start.utc)); // dd/MM/YYYY
+  const dateLong = new Intl.DateTimeFormat('fr-BE', {dateStyle: 'full'}).format(new Date(event.start.utc)); // '21 septembre 2024'
+  const dateFull = new Intl.DateTimeFormat('fr-BE', { dateStyle: 'full' }).format(new Date(event.start.utc)); // samedi 21 septembre 2024
+  const eventUrl = `https://www.eventbrite.be/e/${event.id}`;
+  const name = `CoderDojo 1190 Forest - ${dateShort} Email Campaign`;
+  const subject = `Ne manquez pas ce nouvel événement de CoderDojoBelgium 1190 Forest - ${dateFull}`;
+
+  const bodyMessageTemplate = fs.readFileSync("./campaignBodyTemplate.html").toString();
+  const compiledTemplate = handlebars.compile(bodyMessageTemplate);
+  const bodyMessage = compiledTemplate({
+    dateLong,
+    eventUrl,
+  });
+  // update dates in title (X2) and content (X2/3)
+  await eventBriteClient.updateCampaign({ campaignId: campaignCopy.id, name, subject, bodyMessage });
+  // ask if need to add/enrich list with emails of previous event
+  // if yes, list previous events (default is latest)
+  // Ask for schedule the campain or to send now
+  // done.
+};
+
+main();
